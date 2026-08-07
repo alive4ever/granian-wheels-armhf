ab41600442acbff37989b7971004960d076b4648
alive4ever <alive4ever@users.noreply.github.com>
2026-08-07 23:51:35 +0000

    build: install latest rust toolchain
    Get latest toolchain via sh.rustup.rs

--- build.sh
+++ build.sh
@@ -12,6 +12,18 @@ cd ..
 curl -L -o uv-install.sh https://astral.sh/uv/install.sh
 sh uv-install.sh -v --no-modify-path
 command -v uv || exit 127
+echo "Installing latest rust..."
+curl -Lo ~/rust-bootstrap.sh https://sh.rustup.rs
+. ~/rust-bootstrap.sh
+chmod +x ~/rust-bootstrap.sh
+~/rust-bootstrap.sh -y -v
+echo "Finished installing latest rust..."
+echo "Prepending ~/.cargo/bin to PATH"
+PATH="$HOME/.cargo/bin:$PATH"
+command -v rustc || exit 127
+rustc --version
+command -v cargo || exit 127
+cargo --version
 python3 -m venv ~/venv
 . ~/venv/bin/activate
 pip install -U pip
